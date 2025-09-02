edf629cd8f5fdf8a7b069595a5ae6267f0585a25
Lana El Zaher <lana.alzaher@gmail.com>
2025-09-02 14:46:29 +0300

    Dashboard

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+include("head.php") ?>
+<body class="sb-nav-fixed">
+   <?php 
+include("header.php") ?> 
+        <div id="layoutSidenav">
+
+ <?php 
+include("sidebar.php") ?> 
+            <div id="layoutSidenav_content">
+
+
+<?php 
+include("main.php") ?> 
+
+<?php 
+include("footer.php") ?> 
+</div>
+</div>
+</body>
+</html>
